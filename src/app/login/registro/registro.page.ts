ea0a3b904598e692f8adaa27b602debbf54f86ad
Julio-C-M <mcjjuliocesarr@gmail.com>
2021-11-10 23:21:07 -0600

    Se cambia la interfaz del perfil guardia

--- src/app/login/registro/registro.page.ts
+++ src/app/login/registro/registro.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  OnLogin(): void{
+
+    console.log('Cuenta Creada');
+    this.router.navigateByUrl('/slides-start')
   }
 
 }
